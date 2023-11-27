a87616ebb892e7c327d4cf4f83f19e3743885237
Sanket Rupareliya <sanket.rupareliya@indexnine.com>
2023-11-27 11:05:21 +0530

    added stack child widget for loader

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Works\flutter_rounded_date_picker\example"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Sanket\Documents\DatePicker\flutter_rounded_date_picker\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
